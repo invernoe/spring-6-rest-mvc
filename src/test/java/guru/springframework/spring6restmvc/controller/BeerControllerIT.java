35ac7437afd0f35a5cd00e834a8710bb14008816
invernoe <mahmoudk221999@gmail.com>
2023-06-18 01:00:49 +0300

    Section 15 - Paging and Sorting Add paging parameters

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -55,6 +55,29 @@ class BeerControllerIT {
     }
 
     @Test
+    void testListBeersByStyleAndNameShowInventoryTruePage2() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(BeerController.BEER_PATH)
+                        .queryParam("beerName", "IPA")
+                        .queryParam("beerStyle", BeerStyle.IPA.name())
+                        .queryParam("showInventory", "true")
+                        .queryParam("pageNumber", "2")
+                        .queryParam("pageSize", "50"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(50)))
+                .andExpect(jsonPath("$.[0].quantityOnHand").value(IsNull.notNullValue()));
+    }
+
+    @Test
+    void testListBeersByStyleAndNameShowInventoryDefault() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(BeerController.BEER_PATH)
+                        .queryParam("beerName", "IPA")
+                        .queryParam("beerStyle", BeerStyle.IPA.name()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(310)))
+                .andExpect(jsonPath("$.[0].quantityOnHand").value(IsNull.nullValue()));
+    }
+
+    @Test
     void testListBeersByStyleAndNameShowInventoryFalse() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(BeerController.BEER_PATH)
                         .queryParam("beerName", "IPA")
@@ -213,7 +236,7 @@ class BeerControllerIT {
 
     @Test
     void testListBeers() {
-        List<BeerDTO> dtos = beerController.listBeers(null, null, null);
+        List<BeerDTO> dtos = beerController.listBeers(null, null, false, 1, 25);
         assertThat(dtos.size()).isEqualTo(2413);
     }
 
@@ -222,7 +245,7 @@ class BeerControllerIT {
     @Test
     void testEmptyList() {
         beerRepository.deleteAll();
-        List<BeerDTO> dtos = beerController.listBeers(null, null, null);
+        List<BeerDTO> dtos = beerController.listBeers(null, null, false, 1, 25);
         assertThat(dtos.size()).isEqualTo(0);
     }
 
