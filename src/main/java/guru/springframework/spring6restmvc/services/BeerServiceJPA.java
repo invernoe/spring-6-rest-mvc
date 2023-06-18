2aee7b920de1ae5fad396585021d6e9bc8132869
invernoe <mahmoudk221999@gmail.com>
2023-06-18 05:06:58 +0300

    Section 15 - Paging and Sorting
    Add build page request function
    Committing before implementing full pagination logic

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -7,6 +7,7 @@ import guru.springframework.spring6restmvc.model.BeerStyle;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -22,8 +23,14 @@ public class BeerServiceJPA implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    private static final int DEFAULT_PAGE = 0;
+    private static final int DEFAULT_PAGE_SIZE = 25;
+    private static final int MAX_PAGE_SIZE = 1000;
+
     @Override
     public List<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize) {
+        PageRequest pageRequest = buildPageRequest(pageNumber, pageSize);
+
         List<Beer> beerList;
 
         if (StringUtils.hasText(beerName) && beerStyle == null) {
@@ -46,6 +53,21 @@ public class BeerServiceJPA implements BeerService {
                 .toList();
     }
 
+    private PageRequest buildPageRequest(Integer pageNumber, Integer pageSize) {
+        int queryPageNumber = DEFAULT_PAGE;
+        int queryPageSize = DEFAULT_PAGE_SIZE;
+
+        if(pageNumber != null && pageNumber > 0)
+            queryPageNumber = pageNumber - 1;
+
+        if(pageSize != null && pageSize > 0 && pageSize <= MAX_PAGE_SIZE)
+            queryPageSize = pageSize;
+        else if(pageSize != null && pageSize > 1000)
+            queryPageSize = MAX_PAGE_SIZE;
+
+        return PageRequest.of(queryPageNumber, queryPageSize);
+    }
+
     private List<Beer> listBeersByNameAndStyle(String beerName, BeerStyle beerStyle) {
         return beerRepository.findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle("%" + beerName + "%", beerStyle);
     }
@@ -54,7 +76,7 @@ public class BeerServiceJPA implements BeerService {
         return beerRepository.findAllByBeerStyle(beerStyle);
     }
 
-    List<Beer> listBeersByName(String beerName) {
+    private List<Beer> listBeersByName(String beerName) {
         return beerRepository.findAllByBeerNameIsLikeIgnoreCase("%" + beerName + "%");
     }
 
